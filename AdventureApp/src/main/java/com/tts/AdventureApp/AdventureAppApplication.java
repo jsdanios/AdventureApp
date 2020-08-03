ab7b20413dd8b6ec0c7c1c640285614dd60e7ab1
jsdanios <65199145+jsdanios@users.noreply.github.com>
2020-08-03 11:23:49 -0400

    Add files via upload

---
+++ AdventureApp/src/main/java/com/tts/AdventureApp/AdventureAppApplication.java
@@ -0,0 +1,13 @@
+package com.tts.AdventureApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdventureAppApplication.class, args);
+	}
+
+}
---
+++ AdventureApp/src/test/java/com/tts/AdventureApp/AdventureAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.AdventureApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
