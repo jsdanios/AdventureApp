24db2ca6d6ba5e1c318ea9b2cac036647c824f3f
jsdanios <jsdanios@gmail.com>
2020-08-04 08:52:39 -0400

    manual merge of josiah's branch

--- AdventureApp/src/main/java/com/tts/AdventureApp/service/UserService.java
+++ AdventureApp/src/main/java/com/tts/AdventureApp/service/UserService.java
@@ -1,5 +1,42 @@
 package com.tts.AdventureApp.service;
 
-public class UserService {
+import com.tts.AdventureApp.model.User;
+import com.tts.AdventureApp.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService{
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+    
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public void saveNew(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+    }
+
+    public void saveExisting(User user) {
+        userRepository.save(user);
+    }
+
+    public User getLoggedInUser() {
+        return findByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+    }
     
 }
